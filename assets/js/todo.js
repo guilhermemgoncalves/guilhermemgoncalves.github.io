31253eb6b94a982fc18b1583fc4df9f8d75d3575
Guilherme Marciano Gonçalves <g.marciano.goncalves@avanade.com>
2022-04-27 15:13:16 -0300

    Aula 3

---
+++ assets/js/todo.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+
+const nome = 'Glaucio Daniel';
+let email = 'glauciodaniel@gmail.com';
+console.log(`Nome: ${nome}, Email:${email} `);
+
+function escreve(x){
+    console.log(x);
+}
+
+escreve(`Seja bem vindo ${nome}`);
+escreve("Treinamento HTML,CSS e JS");
