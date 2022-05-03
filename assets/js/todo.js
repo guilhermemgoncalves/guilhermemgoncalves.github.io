015746e49c223d2b8157d42b48584255c8630289
Guilherme Marciano Gonçalves <g.marciano.goncalves@avanade.com>
2022-05-03 15:19:49 -0300

    Aulas 6 e 7

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,52 +1,71 @@
-const db = [
-{
-    id: 1,
-    title: 'Concluir App Conexão Arte',
-    steps: [
-        {step: 'Ajustar textos'},
-        {step: 'Trocar imagens para imagens publicas'},
-        {step: 'Publicar no Expo'},
-        {step: 'Publicar no Expo Store Connect'},
-    ],
-    done: false,
-    dueDate: '2022-05-06',
-    reminder: '2022-05-02 10:00',
-},
+import {
+    addTask,
+    getAllTasks
+} from "./utils/domFunctions.js";
 
-{
-    id:2,
-    title: 'Aula 4 FIAP avanade',
-    steps: [
-        {step: 'Atributos Globais'},
-        {step: 'Estrutura CSS'},        
-    ],
-    done: true,
-    dueDate: '2022-05-28',
-    
-},
+const db = [{
+        id: 1,
+        title: 'Concluir App Conexão Arte',
+        steps: [{
+                step: 'Ajustar textos'
+            },
+            {
+                step: 'Trocar imagens para imagens publicas'
+            },
+            {
+                step: 'Publicar no Expo'
+            },
+            {
+                step: 'Publicar no Expo Store Connect'
+            },
+        ],
+        done: false,
+        dueDate: '2022-05-06',
+        reminder: '2022-05-02 10:00',
+    },
+
+    {
+        id: 2,
+        title: 'Aula 4 FIAP avanade',
+        steps: [{
+                step: 'Atributos Globais'
+            },
+            {
+                step: 'Estrutura CSS'
+            },
+        ],
+        done: true,
+        dueDate: '2022-05-28',
+
+    },
 ];
+getAllTasks(db);
+
 //console.log(db[0].title);
 const form = document.querySelector("#addNewTask");
-form.addEventListener("submit",(e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 const newTask = document.querySelector("#inputTxtNewTask");
-newTask.addEventListener('keyup', (e)=>{
+newTask.addEventListener('keyup', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    if(e.key =="Enter") {
-    if(!newTask.value)
-    {
-        alert("Digite uma nova tarefa para adicionar!")
+    if (e.key == "Enter") {
+        if (!newTask.value) {
+            alert("Digite uma nova tarefa para adicionar!")
+        } else {
+            const d = new Date();
+            const today = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`;
+            db.push({
+                id: Number(db.length) + 1,
+                title: newTask.value,
+                done: false,
+                dueDate: today,
+            });
+            document.querySelector(".tasks").innerHTML='';
+            getAllTasks(db);
+            alert(newTask.value);
+            console.log(db);
+        }
     }
-    else
-    {
-        const d = new Date();
-        const today = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`;
-        db.push({id: Number(db.length) + 1, title: newTask.value, done: false, dueDate: today,});
-        alert(newTask.value);
-        console.log(db); 
-    }
-    }   
-}
-)
+})
--- assets/js/utils/domFunctions.js
+++ assets/js/utils/domFunctions.js
@@ -1,20 +1,33 @@
-export function addTask()
-{
-    alert("Cadastrando uma nova tarefa");
+export function addTask(db, title="") {
+    // Criando uma nova tarefa
+    const task = document.createElement('div');
+    //const id = Number( db.length) + 1
+    task.classList.add('task');
+    task.setAttribute('id', db.id);
+
+    const taskBody = `<div><input type="checkbox" name="" id="check_${db.id}"/></div>
+        <div>
+            <div><span class="title-task">${title ? title : db.title}</span></div>
+          </div>
+        <div>
+                 Star
+        </div>
+    `;
+    task.innerHTML = taskBody;
+    document.querySelector(".tasks").appendChild(task);
 }
-export function removeTask()
-{
+export function removeTask() {
     alert("Removendo uma tarefa");
 }
-export function updateTask()
-{
+export function updateTask() {
     alert("Alterando uma tarefa");
 }
-export function getAllTask()
-{
-    alert("Listando todas tarefas");
-}
-export function getTaskById()
-{
+export function getAllTasks(db) {
+    db.forEach(item => {
+        addTask(item);
+    });    
+}   
+
+export function getTaskById() {
     alert("Exibindo uma tarefa");
 }
